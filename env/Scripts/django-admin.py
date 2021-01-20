1a3ded7e914b456ec9eeb5cedb7625fc6cb67298
nericksx <nerickson@portmorgan.com>
2021-01-19 16:36:51 -0800

    Add project files.

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\neric\Google Drive\Work\python_playground\BasicProject\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
